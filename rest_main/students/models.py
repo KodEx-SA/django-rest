8c52ea67ebd91516019cdf436214b6696681f20f
Ashley K Motsie <motsieashley31@gmail.com>
2026-05-12 09:42:55 +0200

    upd: removed Grade field from models

--- rest_main/students/models.py
+++ rest_main/students/models.py
@@ -4,9 +4,8 @@ class Student(models.Model):
     student_id = models.AutoField(primary_key=True, max_length=10)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    grade = models.CharField(max_length=2)
     branch = models.CharField(max_length=50)
 
     # string representation of the model
     def __str__(self):
-        return f"{self.name} (ID: {self.student_id})"
+        return f"{self.name})"
