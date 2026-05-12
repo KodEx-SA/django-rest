c0b6c7a5629a16a698253724295b43689b220156
Ashley K Motsie <motsieashley31@gmail.com>
2026-05-12 10:05:17 +0200

    upd: stud_id from AutoField to Charfield

--- rest_main/students/models.py
+++ rest_main/students/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Student(models.Model):
-    student_id = models.AutoField(primary_key=True, max_length=10)
+    # student_id = models.AutoField(primary_key=True)
+    student_id = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     course = models.CharField(max_length=50)
 
     # string representation of the model
     def __str__(self):
-        return f"{self.name})"
+        return f"{self.name}"
