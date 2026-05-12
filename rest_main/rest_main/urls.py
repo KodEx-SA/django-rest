2b0b2ea1bc65193e77e05a84e822a30b0004a1e5
Ashley K Motsie <motsieashley31@gmail.com>
2026-05-12 08:46:03 +0200

    fix: api endpoint

--- rest_main/rest_main/urls.py
+++ rest_main/rest_main/urls.py
@@ -4,8 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("students/", include("students.urls")), # web app endpoint
+
     path("api-auth/", include("rest_framework.urls")), # for login/logout in the browsable API
 
     # API endpoints
-    path("api/students/", include("api.urls")), # API endpoint for students
+    path("api/v1/", include("api.urls")), # API endpoint for students
 ]
