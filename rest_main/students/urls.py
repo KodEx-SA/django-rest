ca8ced8a8603c70ac28b75fd1d7fae3a10a5b857
Ashley K Motsie <motsieashley31@gmail.com>
2026-05-12 08:08:10 +0200

    function view for students

--- rest_main/students/urls.py
+++ rest_main/students/urls.py
@@ -1,5 +1,6 @@
 from django.url import path
+from . import views
 
 urlpatterns = [
-    
+    path('students/', views.students, name='students'),
 ]
--- rest_main/students/views.py
+++ rest_main/students/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def students(request):
+    # students = ["Ashley", "John", "Doe"]
+    student = {
+        "name": "Ashley",
+        "age": 25,
+        "grade": "A"
+    }
+    # return HttpResponse(students)
+    return HttpResponse(f"Student: {student['name']}, Age: {student['age']}, Grade: {student['grade']}")
